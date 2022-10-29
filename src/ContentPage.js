fac377a0e623b5756ae0080a7aab563e5efc3b22
Chris Anderson <jchris@gmail.com>
2022-10-29 10:56:34 +0100

    camera uploads

--- src/ContentPage.js
+++ src/ContentPage.js
@@ -4,22 +4,38 @@ import { withIdentity } from './components/Authenticator'
 import { Camera } from 'react-camera-pro'
 import './spinner.css'
 
+function dataURLtoFile (dataurl) {
+  const arr = dataurl.split(',')
+  const mime = arr[0].match(/:(.*?);/)[1]
+  const bstr = atob(arr[1])
+  let n = bstr.length
+  const u8arr = new Uint8Array(n)
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n)
+  }
+  const blob = new Blob([u8arr], { type: mime })
+  return new File([blob], 'camera-image')
+}
+
 export function ContentPage () {
-  const [{ uploadedCarChunks }, uploader] = useUploader()
-  const [file, setFile] = useState(null)
-  const [dataCid, setDataCid] = useState('')
+  const [, uploader] = useUploader()
   const [status, setStatus] = useState('')
   const [error, setError] = useState(null)
+  const [images, setImages] = useState([])
   const camera = useRef(null)
 
   if (!uploader) return null
 
-  const handleUploadSubmit = async e => {
+  const takePhoto = async (e) => {
     e.preventDefault()
+    const imgdata = camera.current.takePhoto()
     try {
+      // Build a DAG from the file data to obtain the root CID.
+      setStatus('encoding')
+      const theFile = dataURLtoFile(imgdata)
       setStatus('uploading')
-      const cid = await uploader.uploadFile(file)
-      setDataCid(cid)
+      const cid = await uploader.uploadFile(theFile)
+      setImages([{ cid: cid, data: imgdata }, ...images])
     } catch (err) {
       console.error(err)
       setError(err)
@@ -28,59 +44,36 @@ export function ContentPage () {
     }
   }
 
-  if (status === 'uploading') {
-    return <Uploading file={file} uploadedCarChunks={uploadedCarChunks} />
-  }
-
-  if (status === 'done') {
-    return error ? <Errored error={error} /> : <Done file={file} dataCid={dataCid} uploadedCarChunks={uploadedCarChunks} />
-  }
+  const printStatus = status === 'done' && error ? error : status
 
   return (
-    <form onSubmit={handleUploadSubmit}>
-      <Camera ref={camera} />
-      <div className='db mb3'>
-        <label htmlFor='file' className='db mb2'>File:</label>
-        <input id='file' className='db pa2 w-100 ba br2' type='file' onChange={e => setFile(e.target.files[0])} required />
-      </div>
-      <button type='submit' className='ph3 pv2'>Upload</button>
-    </form>
+    <div>
+       <p>
+         <button onClick={takePhoto}>Take photo</button> {printStatus}
+       </p>
+       <Camera ref={camera} />
+       <ul className='images'>
+        {images.map(({ cid, data }) => (
+          <ImageListItem key={cid} cid={cid} data={data} />
+        ))}
+       </ul>
+     </div>
   )
 }
 
-const Uploading = ({ file, uploadedCarChunks }) => (
-  <div className='flex items-center'>
-    <div className='spinner mr3 flex-none' />
-    <div className='flex-auto'>
-      <p className='truncate'>Uploading DAG for {file.name}</p>
-      {uploadedCarChunks.map(({ cid, size }) => (
-        <p key={cid.toString()} className='f7 truncate'>
-          {cid.toString()} ({size} bytes)
-        </p>
-      ))}
-    </div>
-  </div>
-)
-
-const Errored = ({ error }) => (
-  <div>
-    <h1 className='near-white'>⚠️ Error: failed to upload file: {error.message}</h1>
-    <p>Check the browser console for details.</p>
-  </div>
-)
-
-const Done = ({ file, dataCid, uploadedCarChunks }) => (
-  <div>
-    <h1 className='near-white'>Done!</h1>
-    <p className='f6 code truncate'>{dataCid.toString()}</p>
-    <p><a href={`https://w3s.link/ipfs/${dataCid}`} className='blue'>View {file.name} on IPFS Gateway.</a></p>
-    <p className='near-white'>Chunks ({uploadedCarChunks.length}):</p>
-    {uploadedCarChunks.map(({ cid, size }) => (
-      <p key={cid.toString()} className='f7 truncate'>
-        {cid.toString()} ({size} bytes)
-      </p>
-    ))}
-  </div>
-)
+function ImageListItem ({ cid, data }) {
+  if (/bagb/.test(`${cid}`)) {
+    return <li key={cid}>CAR cid: {cid}</li>
+  }
+  const imgUrl = `https://w3s.link/ipfs/${cid}`
+  const imgSrc = data || imgUrl
+  return (
+    <li key={cid}>
+      <a href={imgUrl}>
+        <img width="200px" alt='camera output' src={imgSrc} />
+      </a>
+    </li>
+  )
+}
 
 export default withIdentity(ContentPage)
